daaf3b588a92d6db3bcba437d471f1c4c4d1fc84
Shashank S <91001486+shashank-suresh84@users.noreply.github.com>
2023-02-19 10:36:05 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Hi,My Name is Shashank.And My SRN is PES1UG20CS394\n";
+  cout << "Hi,My Name is Shashank";
+  cout << "My SRN is PES1UG20CS394\n";
   return 0;
 }
