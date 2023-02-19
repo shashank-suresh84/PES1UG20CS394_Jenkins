96db349dcc6c492a37d42b8c2cb719faa168c27e
Shashank <shashankss842002@gmail.com>
2023-02-19 09:33:20 +0530

    First commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Hi,My Name is Shashank.And My SRN is PES1UG20CS394\n";
+  return 0;
+}
